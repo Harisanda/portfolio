5085981a040d9cdf39facf2b684fb7913d76253c
Harisanda <harisandaramarovao@gmail.com>
2026-01-22 20:43:47 +0300

    fix: slider responsive with swiper

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,63 +1,57 @@
 import React, { useEffect, useRef } from 'react'
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import { Swiper, SwiperSlide } from 'swiper/react';
 import './style/Projects.css'
 import { data } from '../utils/data';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
 
 
 const Projects = () => {
-
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        pauseOnHover: true,
-        responsive : [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                }
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                }
-            },
-        ]
-    };
 
   return (
     <section className='projects' id='projects'>
         <h2>PROJETS</h2>
         <div className="projects-content">
-            <Slider {...settings}>
-            {
-                data.map(item => (
-                
-                    <div className='card-project'>
-                        <div className='project-img'>
-                            <img src={item.img} alt="projet-image"/>
-                        </div>
-                        <div className='description'>
-                            <h3>{item.titre}</h3>
-                            <p>{item.description.slice(0,130)}...</p>
-                        </div>
-                        <div className='btn'>
-                            <button>Voir plus</button>
-                        </div>
-                    </div>
-                ))
-            }
-            </Slider>
+            <Swiper 
+                modules={[ Autoplay]}
+                spaceBetween={20}
+                pagination={{ clickable: true }}
+                loop={true}
+                autoplay={{
+                    delay: 2000,
+                    disableOnInteraction: false,
+                    pauseOnMouseEnter: true,
+                }}
+                breakpoints={{
+                    0: { slidesPerView: 1 },
+                    768: { slidesPerView: 2 },
+                    1024: { slidesPerView: 3 },
+                }}
+            >
+                {data.map((item, index) => (
+                        <SwiperSlide key={index}>
+                            <div className="card-project">
+                                <div className="project-img">
+                                    <img src={item.img} alt="projet-image" />
+                                </div>
+
+                                <div className="description">
+                                    <h3>{item.titre}</h3>
+                                    <p>{item.description.slice(0, 130)}...</p>
+                                </div>
+
+                                <div className="btn">
+                                    <button>Voir plus</button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
+            </Swiper>
         </div>
     </section>
   )
